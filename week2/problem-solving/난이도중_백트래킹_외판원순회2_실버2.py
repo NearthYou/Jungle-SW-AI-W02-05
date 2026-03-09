9eb004630c2c664b0c38ebfe7802e66eed5fafa6
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-09 21:53:55 +0900

    Solving Bakjoon Medium Level

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,26 @@
 # 백트래킹 - 외판원 순회 2 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10971
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [False] * N
+visited[0] = True
+min_cost = float('inf')
+
+def dfs(now, cnt, cost):
+    global min_cost
+    if(min_cost <= cost):
+        return
+    if(cnt == N):
+        if arr[now][0] != 0:
+            min_cost = min(min_cost, cost + arr[now][0])
+        return
+    
+    for next in range(N):
+        if not visited[next] and arr[now][next] != 0:
+            visited[next] = True
+            dfs(next, cnt + 1, cost + arr[now][next])
+            visited[next] = False
+
+dfs(0, 1, 0)
+print(min_cost)
--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,16 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+from itertools import permutations
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+max_diff = 0
+for p in permutations(nums):
+    cur_diff = sum(abs(p[i] - p[i+1]) for i in range(N - 1))
+
+    if cur_diff > max_diff:
+        max_diff = cur_diff
+
+print(max_diff)
