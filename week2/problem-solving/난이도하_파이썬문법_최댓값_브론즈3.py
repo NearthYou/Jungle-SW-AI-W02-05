0e6b1a89fc51bece34de410973c920209d3a3385
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-09 11:57:57 +0900

    Update 난이도하_파이썬문법_최댓값_브론즈3.py

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,13 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+nums = list()
+for i in range(9):
+    n = int(input())
+    nums.append(n)
+
+max_num = max(nums)
+max_idx = nums.index(max_num)
+
+print(max_num)
+print(max_idx)
