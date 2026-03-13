1235724cdb637fc45c7f1405b7441f83f964267e
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-13 23:26:33 +0900

    Update 난이도중_문자열_IPv6_실버1.py

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,28 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+ip = input()
+
+# :: 를 기준으로 분리
+if "::" in ip:
+    left, right = ip.split("::")
+
+    left_parts = left.split(":") if left else []
+    right_parts = right.split(":") if right else []
+    
+
+    missing_count = 8 - (len(left_parts) + len(right_parts))
+    middle_parts = ["0000"] * missing_count
+    
+    full_parts = left_parts + middle_parts + right_parts
+else:
+
+    full_parts = ip.split(":")
+
+
+restored_parts = []
+for part in full_parts:
+
+    restored_parts.append(part.zfill(4))
+
+print(":".join(restored_parts))
