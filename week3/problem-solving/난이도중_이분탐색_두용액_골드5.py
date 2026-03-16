83ae929e7571f28f0895df99a21959d6b363b6f5
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-17 02:37:12 +0900

    Update 난이도중_이분탐색_두용액_골드5.py

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,29 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+# 둘을 더했을 때 0에 가장 가까운 값 찾기
+
+n = int(input())
+nums = list(map(int, input().split()))
+nums = sorted(nums)
+
+left = 0
+right = len(nums) - 1
+min_abs_sum = float('inf')
+ans = [0, 0]
+
+while left < right:
+    current_sum = nums[left] + nums[right]
+    
+    if abs(current_sum) < min_abs_sum:
+        min_abs_sum = abs(current_sum)
+        ans = [nums[left], nums[right]]
+    
+    if current_sum < 0:
+        left += 1
+    elif current_sum > 0:
+        right -= 1
+    else:
+        break
+
+print(ans[0], ans[1])
