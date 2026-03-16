c1a96c1ee2706e6c2526cd76a97577250940b3c0
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-16 10:48:13 +0900

    Update 난이도하_큐_카드2_실버4.py

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,22 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+# 제일 위에 있는 카드를 버린다.
+# 그 다음 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
+# 카드가 한장 남을 때까지 반복
+
+# 1234 -> 234 -> 342
+# 제일 먼저 넣은거 pop
+# 그 다음 숫자는 pop 한것 다시 append
+
+from collections import deque
+
+n = int(input())
+d = deque([x + 1 for x in range(n)])
+
+while len(d) != 1:
+    d.popleft()
+    pop_item = d.popleft()
+    d.append(pop_item)
+
+print(d.pop())
