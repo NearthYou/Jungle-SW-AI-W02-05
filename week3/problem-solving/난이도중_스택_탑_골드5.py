77451e377bd9345aaf6e8e93339a15cd2dba69de
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-17 03:25:04 +0900

    Update 난이도중_스택_탑_골드5.py

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,18 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+# 하나의 탑에서 발사된 레이저 신호
+# N개의 탑들의 높이가 입력으로 주어짐
+# 숫자에서 왼쪽으로
+
+n = int(input())
+nums = list(map(int, input().split()))
+rs = [0] * n
+
+stack = []
+for i in range(n - 1, -1, -1):
+    while stack and nums[i] > nums[stack[-1]]:
+        idx = stack.pop()
+        rs[idx] = i + 1
+    stack.append(i)
+print(*rs)
