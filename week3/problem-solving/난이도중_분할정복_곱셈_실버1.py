5eede6c00123eb74f49e5d0feadd8558133fbed8
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-17 22:33:25 +0900

    Update 난이도중_분할정복_곱셈_실버1.py

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,15 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+def divide_conquer(x, y, z):
+    x %= z
+    if y == 0:
+        return 1
+    half = divide_conquer(x, y // 2, z)
+
+    if y % 2 == 0:
+        return (half * half) % z
+    else:
+        return (half * half * x) % z
+
+x, y, z = map(int, input().split())
+print(divide_conquer(x, y, z))
