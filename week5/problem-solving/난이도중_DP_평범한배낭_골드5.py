a8d96a9328f69fc654f4384f306da9fc142091ce
시원 <96858320+NearthYou@users.noreply.github.com>
2026-03-30 14:27:45 +0900

    Update 난이도중_DP_평범한배낭_골드5.py

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,38 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+items = []
+for _ in range(n):
+    items.append(list(map(int, input().split())))
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    weight, value = items[i - 1]
+    for j in range(1, k + 1):
+        if j >= weight:
+            dp[i][j] = max(dp[i- 1][j], dp[i - 1][j - weight] + value)
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+print(dp[n][k])
+
+# import sys
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+
+# dp = [0] * (k + 1)
+
+# for _ in range(n):
+#     w, v = map(int, input().split())
+    
+#     for j in range(k, w - 1, -1):
+#         dp[j] = max(dp[j], dp[j - w] + v)
+
+# print(dp[k])
